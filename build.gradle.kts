e924a64e2709483c588bee64922fafd035dcb7b3
Niko Diamadis <niko@cyb3rko.de>
2025-01-01 18:52:00 -0500

    chore: update backpack

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@ import org.gradle.kotlin.dsl.libs
 
 plugins {
     alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.android.library) apply false
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.androidx.navigation.safeargs) apply false
 }
